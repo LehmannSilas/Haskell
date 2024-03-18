2c4bf54ec16a9a34e4f2f51226ca2541ea983885
silasVM <silaslehmann5@gmail.com>
2024-03-18 19:58:45 +0100

    first functional 'self made' by Copilots help

--- haskell01/second.hs
+++ haskell01/second.hs
@@ -1,7 +1,9 @@
+import System.IO
+
 addMe :: Integer -> Integer -> Integer
 addMe x y = x + y
 
 main :: IO ()
-main =  do
-putStr "Sum of x + y = "
-print(addMe 10 25)
+main = do
+    putStrLn "Sum of x + y = "
+    print (addMe 10 25)
