8257c7e612ce422706127cbcaf51bf2eb6be65f2
silasVM <silaslehmann5@gmail.com>
2024-03-19 20:42:38 +0100

    first Tasks Done

---
+++ haskell02 - Aufgaben01/03_Funktionen.hs
@@ -0,0 +1,19 @@
+-- Test
+test :: Int -> Int
+test x = x
+
+-- Übungen
+add5 :: Int -> Int
+add5 x = x + 5
+
+hoch3 :: Int -> Int
+hoch3 x = x * x * x
+
+hoch3E :: Int -> Int
+hoch3E x = x^3
+
+inc :: Int -> Int
+inc x = x + 1
+
+istZweistellig :: Int -> Bool
+istZweistellig x = x >= 10 && x < 100
